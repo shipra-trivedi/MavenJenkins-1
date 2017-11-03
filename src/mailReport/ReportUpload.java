a28d93388fcd8f836b1daa975ffeb9b10a1098ac
John Doe <john@sample.com>
2017-11-03 16:44:43 +0530

    demo 3

--- src/mailReport/ReportUpload.java
+++ src/mailReport/ReportUpload.java
@@ -1,59 +1,120 @@
-//package mailReport;
-//
-//import com.dropbox.core.*;
-//import java.io.*;
-//import java.util.Locale;
-//
-//public class ReportUpload {
-//    public static void main(String[] args) throws IOException, DbxException {
-//        // Get your app key and secret from the Dropbox developers website.
-//        final String APP_KEY = "INSERT_APP_KEY";
-//        final String APP_SECRET = "INSERT_APP_SECRET";
-//
-//        DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
-//
-//        DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",
-//            Locale.getDefault().toString());
-//        DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
-//
-//        // Have the user sign in and authorize your app.
-//        String authorizeUrl = webAuth.start();
-//        System.out.println("1. Go to: " + authorizeUrl);
-//        System.out.println("2. Click \"Allow\" (you might have to log in first)");
-//        System.out.println("3. Copy the authorization code.");
-//        String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-//
-//        // This will fail if the user enters an invalid authorization code.
-//        DbxAuthFinish authFinish = webAuth.finish(code);
-//        String accessToken = authFinish.accessToken;
-//
-//        DbxClient client = new DbxClient(config, accessToken);
-//
-//        System.out.println("Linked account: " + client.getAccountInfo().displayName);
-//
-//        File inputFile = new File("working-draft.txt");
-//        FileInputStream inputStream = new FileInputStream(inputFile);
-//        try {
-//            DbxEntry.File uploadedFile = client.uploadFile("/magnum-opus.txt",
-//                DbxWriteMode.add(), inputFile.length(), inputStream);
-//            System.out.println("Uploaded: " + uploadedFile.toString());
-//        } finally {
-//            inputStream.close();
-//        }
-//
-//        DbxEntry.WithChildren listing = client.getMetadataWithChildren("/");
-//        System.out.println("Files in the root path:");
-//        for (DbxEntry child : listing.children) {
-//            System.out.println("	" + child.name + ": " + child.toString());
-//        }
-//
-//        FileOutputStream outputStream = new FileOutputStream("magnum-opus.txt");
-//        try {
-//            DbxEntry.File downloadedFile = client.getFile("/magnum-opus.txt", null,
-//                outputStream);
-//            System.out.println("Metadata: " + downloadedFile.toString());
-//        } finally {
-//            outputStream.close();
-//        }
-//    }
-//}
+/*package mailReport;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.*;
+import com.google.api.services.drive.Drive;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReportUpload {
+    *//** Application name. *//*
+    private static final String APPLICATION_NAME =
+        "Drive API Java Quickstart";
+
+    *//** Directory to store user credentials for this application. *//*
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+        System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+    *//** Global instance of the {@link FileDataStoreFactory}. *//*
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    *//** Global instance of the JSON factory. *//*
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    *//** Global instance of the HTTP transport. *//*
+    private static HttpTransport HTTP_TRANSPORT;
+
+    *//** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/drive-java-quickstart
+     *//*
+    private static final List<String> SCOPES =
+        Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    *//**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     *//*
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+            ReportUpload.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    *//**
+     * Build and return an authorized Drive client service.
+     * @return an authorized Drive client service
+     * @throws IOException
+     *//*
+    public static Drive getDriveService() throws IOException {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Build a new authorized API client service.
+        Drive service = getDriveService();
+
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+             .setPageSize(10)
+             .setFields("nextPageToken, files(id, name)")
+             .execute();
+        List<File> files = result.getFiles();
+        if (files == null || files.size() == 0) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
+    }
+
+}*/
